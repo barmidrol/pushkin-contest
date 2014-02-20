50897a2ff5d1378570a46d773d6c93fbd43879e6
ars <arsemchik@gmail.com>
2014-02-20 23:15:07 +0300

    Refactoring registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
 
-  QUESTION = 'Буря мглою небо кроет, Вихри %word% крутя'.freeze
-  ANSWER = 'снежные'.freeze
-  RESPONSE_IDLE_TIME = 10.seconds.freeze
   NUM_USERS = 10.freeze
 
 
@@ -14,26 +11,7 @@ class UsersController < ApplicationController
   def registration
     @user = User.new(user_params)
 
-    if @user.valid?
-      #@user.generate_token
-      uri = URI(@user.url)
-      request = Net::HTTP::Post.new(uri.path)
-      request.set_form_data('question' => QUESTION)
-
-      response = Net::HTTP.start(uri.hostname, uri.port, read_timeout: RESPONSE_IDLE_TIME) do |http|
-        http.request(request)
-      end
-
-      @user.errors.add(:base, 'Answer is false!')  if response.body.downcase != ANSWER #response.body.include?(ANSWER)
-
-      @user.save if @user.errors.empty?
-    end
-
-  rescue Timeout::Error => e
-    @user.errors.add(:base, 'Time is over!')
-
-  rescue Exception => e
-    @user.errors.add(:base, e.message)
+    redirect_to rating_path and return if @user.registration
   end
 
   def test_bot
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,46 @@
 class User < ActiveRecord::Base
 
+  QUESTION = 'Буря мглою небо кроет, Вихри %word% крутя'.freeze
+  ANSWER = 'снежные'.freeze
+  RESPONSE_IDLE_TIME = 10.seconds.freeze
+
   validate :username, uniqueness: true, present: true
-  validate :url, present: true
+  validates :url, presence: true, url: true
+  validate :token, uniqueness: true
 
   scope :rating, -> { order('rating desc') }
 
   def generate_token
-    self.token = loop do
-      random_token = SecureRandom.urlsafe_base64(nil, false)
-      break random_token unless User.exists?(token: random_token)
+    self.token = SecureRandom.urlsafe_base64(nil, false)
+  end
+
+  def registration
+    uri = URI(self.url)
+    request = Net::HTTP::Post.new(uri.path)
+    request.set_form_data('question' => QUESTION)
+
+    response = Net::HTTP.start(uri.hostname, uri.port, read_timeout: RESPONSE_IDLE_TIME) do |http|
+      http.request(request)
     end
+
+    if response.body.downcase == ANSWER #response.body.include?(ANSWER)
+      self.generate_token
+      self.save
+    else
+      self.errors.add(:base, 'Answer is false!')
+    end
+
+    self.errors.empty?
+
+
+  rescue Net::ReadTimeout
+    self.errors.add(:base, 'Time is over')
+    false
+
+  rescue Exception => e
+    self.errors.add(:base, e.message)
+    false
   end
 
 end
+
