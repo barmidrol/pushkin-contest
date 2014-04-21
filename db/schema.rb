fe08d5e5c76640cefccf86b82fec33c677f9d893
Valentine Zavadsky <saicheg@gmail.com>
2014-04-21 12:09:37 +0300

    fix types for tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317193131) do
+ActiveRecord::Schema.define(version: 20140421090852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20140317193131) do
   end
 
   create_table "tasks", force: true do |t|
-    t.string   "question"
-    t.string   "answer"
+    t.text     "question"
+    t.text     "answer"
     t.boolean  "answered",   default: false
     t.integer  "level"
     t.integer  "user_id"
