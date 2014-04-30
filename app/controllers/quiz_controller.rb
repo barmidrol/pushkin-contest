748ffa6da0ba29d234774b9c501d4b59e5e39de0
Valentine Zavadsky <saicheg@gmail.com>
2014-05-01 00:10:58 +0300

    fix bug

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -16,7 +16,7 @@ class QuizController < ApplicationController
     @task = Task.find params[:task_id]
     if @task.answer.mb_chars.downcase.to_s.strip == @answer.mb_chars.downcase.to_s.strip
       ActiveRecord::Base.transaction do
-        t = Task.where(answered: false, id: params[:task_id])
+        t = Task.find_by(answered: false, id: params[:task_id])
         if t
           t.update_attributes(answered: true, user_id: user.id)
           user.increment! :rating, 1
