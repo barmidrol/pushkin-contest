23ff7040145beb6c9063d17ac7fa880d959f357c
Valentine Zavadsky <saicheg@gmail.com>
2014-05-03 11:49:48 +0300

    remove some logs from pushkin

--- app/workers/send_task_to_users.rb
+++ app/workers/send_task_to_users.rb
@@ -6,10 +6,6 @@ class SendTaskToUsers
     task = Task.find(task_id)
     users = User.where(level: task.level).order('random()').not_winners
 
-    p "TaskSender for level #{task.level} has #{users.count} users"
-    users.each do |u|
-      TaskSender.perform_async(u.id, task.id)
-      p "TaskSender to user #{u.id}  with #{u.url} was started"
-    end
+    users.each { |u| TaskSender.perform_async(u.id, task.id) }
   end
 end
