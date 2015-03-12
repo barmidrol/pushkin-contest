f09c77a8ceab2b53c6e7beec5e4c332fddf9112d
Valentine Zavadsky <saicheg@gmail.com>
2015-03-12 11:45:44 +0300

    add sidekiq role for production user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :branch, 'master'
 set :rails_env, 'production'
 set :unicorn_env, 'production'
 
-server '188.166.25.60',  user: 'deployer', roles: %w{app web db}
+server '188.166.25.60',  user: 'deployer', roles: %w{app web db sidekiq}
