4ba9afe9e5d449c83b07907f2922190bdc9033a6
Valentine Zavadsky <saicheg@gmail.com>
2015-03-18 09:34:29 +0300

    fix levels

--- app/workers/cron_worker.rb
+++ app/workers/cron_worker.rb
@@ -5,7 +5,7 @@ class CronWorker
   recurrence { secondly(20) }
 
   def perform
-    (1..5).to_a.each do |level|
+    (1..8).to_a.each do |level|
       task = TaskCreator::Factory.factory(level).create
 
       raise "Task is not saved: #{task.to_s}" if task.id.blank?
