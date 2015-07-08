87de1e5ed4111c8a6e1f1548c64741387a8d46b7
Yury Kaliada <fut.wrk@gmail.com>
2015-07-08 19:58:21 +0300

    2.2.2 ruby
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :branch, 'master'
 set :rails_env, 'production'
 set :unicorn_env, 'production'
 
-server '188.166.25.60',  user: 'deployer', roles: %w{app web db sidekiq}
+server '46.101.238.197',  user: 'deployer', roles: %w{app web db sidekiq}
