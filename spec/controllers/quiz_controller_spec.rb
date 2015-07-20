6a7071824d8c9a3f56008216e6a7f3e19ca0de68
Oleg Zubchenko <RedGreenBlueDiamond@gmail.com>
2015-07-21 02:27:30 +0300

    close vulnerabilities

--- spec/controllers/quiz_controller_spec.rb
+++ spec/controllers/quiz_controller_spec.rb
@@ -12,6 +12,13 @@ describe QuizController do
       errors.should include("User does not exist")
     end
 
+    it 'should render an error message if task not found' do
+      post :answer, task_id: -1, format: :json
+      errors = JSON.parse(response.body)["errors"]
+      errors.should_not be_blank
+      errors.should include("Task does not exist")
+    end
+
     it 'should render an error message if task is answered' do
       task.stub(answered?: true)
       QuizAnswerRequest.any_instance.stub(task: task)
@@ -20,6 +27,28 @@ describe QuizController do
       errors = JSON.parse(response.body)["errors"]
       errors.should_not be_blank
       errors.should include("Task is expired")
+    end
+
+    it 'should render an error message if task is outdated' do
+      task.stub(created_at: 1.day.ago)
+      QuizAnswerRequest.any_instance.stub(task: task)
+
+      post :answer, formate: :json
+      errors = JSON.parse(response.body)["errors"]
+      errors.should_not be_blank
+      errors.should include("Task is outdated")
+    end
+
+    it 'should render an error message if task level differs from user level' do
+      user = User.new
+      user.stub(level: 1)
+      task.stub(level: 2)
+      QuizAnswerRequest.any_instance.stub(task: task, user: user)
+
+      post :answer, format: :json
+      errors = JSON.parse(response.body)["errors"]
+      errors.should_not be_blank
+      errors.should include("Task level mismatch")
     end
 
     it 'should render an error if answer is blank' do
