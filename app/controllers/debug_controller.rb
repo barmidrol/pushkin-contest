7c7c09aa9e53a6ab02cabd26ca7bf86824fcc6ee
barmidrol <barmidrol@gmail.com>
2016-01-07 18:39:34 +0300

    Add level reset for users

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -5,14 +5,15 @@ class DebugController < ApplicationController
     @level = params[:level] ? params[:level].to_i : 1
     
     @user = User.find_by_token(@token)
-
+    message = ''
     if !@user
-      render json: { error: "User with this token doesn't exist" }
+      message = "User with this token doesn't exist"
     elsif !@level.between? 1, 8
-      render json: { error: "Level should be in range [1..8]" }
+      message = "Level should be in range [1..8]"
     else
       @user.update_attributes(rating: (@level - 1) * 100)
-      render json: { message: "Level successfully updated" }
+      message = "Level successfully updated" 
     end
+    render json: { message: message }
   end
 end
